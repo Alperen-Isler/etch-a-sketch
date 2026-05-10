70343e394509c498f8ec18e7df48a9a1c5594669
Alperen-Isler <alperenislerdev@gmail.com>
2026-05-10 17:53:31 +0200

    Add Eventlisteners to color buttons
    Make every color button change the drawing color to clicked color of the button.

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 const container = document.querySelector(".container");
+const cherryRed = document.querySelector(".cherry-red");
+const creamWhite = document.querySelector(".cream-white");
+const skyBlue = document.querySelector(".sky-blue");
+const mustardYellow = document.querySelector(".mustard-yellow");
+const mintGreen = document.querySelector(".mint-green");
+const deepNavy = document.querySelector(".deep-navy");
+const softOrange = document.querySelector(".soft-orange");
+const lavenderPurple = document.querySelector(".lavender-purple");
 
 createDivs();
+
+let color;
+
+cherryRed.addEventListener("click", function(e){
+    color = "cherry-red";
+});
+
+creamWhite.addEventListener("click", function(e){
+    color = "cream-white";
+});
+
+skyBlue.addEventListener("click", function(e){
+    color = "sky-blue";
+});
+
+mustardYellow.addEventListener("click", function(e){
+    color = "mustard-yellow";
+});
+
+mintGreen.addEventListener("click", function(e){
+    color = "mint-green";
+});
+
+deepNavy.addEventListener("click", function(e){
+    color = "deep-navy";
+});
+
+softOrange.addEventListener("click", function(e){
+    color = "soft-orange";
+});
+
+lavenderPurple.addEventListener("click", function(e){
+    color = "lavender-purple";
+});
 
 function createDivs (){
     for (let i = 0; i < 16; i++){
@@ -19,10 +61,7 @@ let drawDivs = document.querySelectorAll(".drawDiv");
 
 drawDivs.forEach(drawDiv => {
     drawDiv.addEventListener("mouseover", function(e){
-        drawDiv.classList.add("black");
-    });
-    drawDiv.addEventListener("mouseleave", function(e){
-        drawDiv.classList.remove("black");
+        drawDiv.classList.add(color);
     });
 });
 
